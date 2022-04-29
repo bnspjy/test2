01a23c3cc37545764b0c2c30ac3f4a0a7368297c
Hyojin Jeon <jhyoj930@gmail.com>
2022-04-29 23:46:41 +0900

    222

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("asdasd");
-		System.out.println("효진 개발");
+		System.out.println("효진 개발2");
 		
 	}
 
